f15d03f962273b99be73ff2a265e9b95420ca279
Dr. Rhiannon Peery <rmpeery@gmail.com>
2025-01-27 15:13:17 -0800

    Update fasta2SAF.sh
    Create a dummy SAF file from a fasta sequence - useful if using the CDS for read mapping.

--- fasta2SAF.sh
+++ fasta2SAF.sh
@@ -1,3 +1,7 @@
+#########################
+# From TRINITY assembly #
+#########################
+
 !#/usr/bin/bash
 
 ###
@@ -22,3 +26,22 @@ cat head.txt temp.SAF >outfile.SAF
 #check file
 head outfile.SAF
 rm head.txt temp.SAF chr.txt
+
+##############################
+# From gtf for gene features #
+##############################
+# this example uses the gtf avail. from Psme.1_0 on the TreeGenesDB
+
+!#/usr/bin/bash
+
+bioawk -c fastx '{ print $name, length($seq) }' <Psme.1_0.cds.fa >temp.SAF; # could use faidx from samtools as alt. if bioawk is not avail.
+grep gene Psme.1_0.gtf >Psme.1_0.CDS.gtf;
+paste temp.SAF Psme.1_0.CDS.gtf >tmp.SAF;
+rm temp.SAF Psme.1_0.CDS.gtf;
+awk '{print $1, $1, "1", $2, "+"}' tmp.SAF >tmp && mv tmp tmp.SAF;
+echo "GeneID Chr Start End Strand" >head.txt;
+cat head.txt tmp.SAF >Psme.1_0.CDS.SAF;
+# check file
+head Psme.1_0.CDS.SAF;
+rm tmp.SAF head.txt;
+sed -i 's/ /\t/g' Psme.1_0.CDS.SAF;
